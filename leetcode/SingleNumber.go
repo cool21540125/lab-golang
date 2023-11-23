6ae8369576806867dfc51b70b7c5f640fbbeb98d
Tony Chou <cool21540125@gmail.com>
2023-11-24 01:27:43 +0800

    impl: Remove useless print

--- leetcode/SingleNumber.go
+++ leetcode/SingleNumber.go
@@ -1,12 +1,10 @@
 package leetcode
 
-import "fmt"
-
 func singleNumber(nums []int) int {
 	var res int
 	for i := 0; i < len(nums); i++ {
 		res ^= nums[i]
-		fmt.Println(res)
+		// fmt.Println(res)
 	}
 	return res
 }
