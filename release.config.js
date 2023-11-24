921cf7e22d32509d4a71f81d935e21f6119745cd
TonyChou <cool21540125@gmail.com>
2023-11-24 16:55:38 +0800

    ci: Adds semantic-release

--- release.config.js
+++ release.config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
                 assets: ['CHANGELOG.md', 'package.json'],
                 message:
-                    'chore(release): ${nextRelease.version} [skip ci]',
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
         '@semantic-release/github',
